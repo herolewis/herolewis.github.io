36afa0d8c3588b13fabfb4c34c30ff55b957283e
herolewis <lewiskycc@hotmail.com>
2019-08-03 22:26:15 +0800

    Updates

---
+++ precache-manifest.2bc5cd25206c6dcfe76f9a71ffd2592d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf6fd02897d5d36b7080403ffbfb65d",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "d6313e5be349d33dbaa6",
+    "url": "/web/static/css/2.862259ab.chunk.css"
+  },
+  {
+    "revision": "8232f22f8da4b935b867",
+    "url": "/web/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "d6313e5be349d33dbaa6",
+    "url": "/web/static/js/2.36e15036.chunk.js"
+  },
+  {
+    "revision": "8232f22f8da4b935b867",
+    "url": "/web/static/js/main.af3cbcfd.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  }
+]);
